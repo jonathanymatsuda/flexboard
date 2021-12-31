085027ec8fc510be1edb497282a9f15cba5326a6
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2021-12-31 02:07:56 +0000

    Changed form layout

--- client/app.jsx
+++ client/app.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 // import Home from './pages/home';
 import PageHeader from './components/page-header';
+import WorkspaceForm from './components/workspace-form';
 
 export default class App extends React.Component {
   render() {
     return (
-    <PageHeader/>
+      <>
+        <PageHeader />
+        <WorkspaceForm/>
+      </>
     );
   }
 }
--- client/components/page-header.jsx
+++ client/components/page-header.jsx
@@ -9,9 +9,7 @@ export default function PageHeader() {
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
             <div className="relative flex items-center justify-between h-16">
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex-shrink-0 flex items-center">
-                  <h3 className="font-bold text-white">Flexboard</h3>
-                </div>
+                <h3 className="font-bold text-white">Flexboard</h3>
               </div>
             </div>
           </div>
