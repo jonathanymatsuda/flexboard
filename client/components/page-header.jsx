cc4e413b14e33fa34f2f07486a56bd3bca43732c
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2022-01-01 05:16:01 +0000

    Header finalized for core features

--- client/components/page-header.jsx
+++ client/components/page-header.jsx
@@ -1,5 +1,15 @@
 import React, { Fragment } from 'react';
 import { Disclosure } from '@headlessui/react';
+import { MenuIcon, XIcon } from '@heroicons/react/outline';
+
+const navigation = [
+  { name: 'Active Workspaces', href: '#', current: true },
+  { name: 'Boards', href: '#', current: false }
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
+}
 
 export default function PageHeader() {
   return (
@@ -8,8 +18,45 @@ export default function PageHeader() {
         <>
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
             <div className="relative flex items-center justify-between h-16">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button */}
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open
+                    ? (
+                    <XIcon className="block h-6 w-6" aria-hidden="true" />
+                      )
+                    : (
+                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                      )}
+                </Disclosure.Button>
+              </div>
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                <h3 className="font-bold text-white">Flexboard</h3>
+                <div className="flex-shrink-0 flex items-center">
+                  <img
+                    className="h-8 w-auto mr-1"
+                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                    alt="Workflow"
+                  />
+                  <h2 className='text-white font-bold'>Flexboard</h2>
+                </div>
+                <div className="hidden sm:block sm:ml-6">
+                  <div className="flex space-x-4">
+                    {navigation.map(item => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current ? 'bg-indigo-700 text-white' : 'text-gray-300 hover:bg-indigo-700 hover:text-white',
+                          'px-3 py-2 rounded-md text-sm font-medium'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
               </div>
             </div>
           </div>
