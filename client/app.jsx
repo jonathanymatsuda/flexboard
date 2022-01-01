459d7a389e4fc58e79ca3a40067ef367485ef7d4
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2022-01-01 07:50:00 +0000

    WorkspaceListView component made and functional

--- client/app.jsx
+++ client/app.jsx
@@ -9,7 +9,7 @@ export default class App extends React.Component {
       <>
         <PageHeader />
         <WorkspaceForm />
-        <WorkspaceListView />
+        <WorkspaceListView/>
       </>
     );
   }
--- client/pages/workspace-home.jsx
+++ client/pages/workspace-home.jsx
@@ -1,59 +1,44 @@
-// import React from 'react';
+import React from 'react';
 
-// const projects = [
-//   { name: 'Graph API', initials: 'GA', href: '#', members: 16, bgColor: 'bg-pink-600' },
-//   { name: 'Component Design', initials: 'CD', href: '#', members: 12, bgColor: 'bg-purple-600' },
-//   { name: 'Templates', initials: 'T', href: '#', members: 16, bgColor: 'bg-yellow-500' },
-//   { name: 'React Components', initials: 'RC', href: '#', members: 8, bgColor: 'bg-green-500' }
-// ];
+export default class WorkspaceListView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      workspaces: []
+    };
+  }
 
-// function classNames(...classes) {
-//   return classes.filter(Boolean).join(' ');
-// }
+  componentDidMount() {
+    fetch('/api/workspaces')
+      .then(res => res.json())
+      .then(workspacePanels => this.setState({ workspaces: workspacePanels }))
+      .then(err => console.error(err));
+  }
 
-// export default class WorkspaceListView extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.setState = {
-//       workspaces: []
-//     };
-//   }
-
-//   componentDidMount() {
-//     fetch('/api/workspaces')
-//       .then(res => res.json())
-//       .then(workspacePanels => this.setState({ workspaces: workspacePanels }))
-//       .then(err => console.error(err));
-//   }
-
-// //   render() {
-// //     return (
-// //     <div>
-// //       <h2 className="text-gray-500 text-xs font-medium uppercase tracking-wide">Active Workspaces</h2>
-// //       <ul role="list" className="mt-3 grid grid-cols-1 gap-5 sm:gap-6 sm:grid-cols-2 lg:grid-cols-4">
-// //         {this.state.workspaces.map(workspace => (
-// //           <li key={workspace.workspaceId} className="col-span-1 flex shadow-sm rounded-md">
-// //             <div
-// //               className={classNames(
-// //                 project.bgColor,
-// //                 'flex-shrink-0 flex items-center justify-center w-16 text-white text-sm font-medium rounded-l-md'
-// //               )}
-// //             >
-// //               {/* `{workspace/.initials} */}
-// //             </div>
-// //             <div className="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
-// //               <div className="flex-1 px-4 py-2 text-sm truncate">
-// //                 <a href={project.href} className="text-gray-900 font-medium hover:text-gray-600">
-// //                   {project.name}
-// //                 </a>
-// //                 <p className="text-gray-500">{project.members} Members</p>
-// //               </div>
-// //             </div>
-// //           </li>
-// //         ))}
-// //       </ul>
-// //     </div>
-// //     );
-// //   }
-// // }
-// //
+  render() {
+    return (
+    <div className='p-8'>
+      <h2 className="text-gray-500 text-xs font-medium uppercase tracking-wide">Active Workspaces</h2>
+      <ul role="list" className="mt-3 grid grid-cols-1 gap-5 sm:gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        {this.state.workspaces.map(workspace => (
+          <li key={workspace.workspaceId} className="col-span-1 flex shadow-sm rounded-md">
+            <div
+              className='flex-shrink-0 flex items-center justify-center w-16 text-white text-sm font-medium rounded-l-md bg-blue-500'
+            >
+              WS{workspace.workspaceId}
+            </div>
+            <div className="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
+              <div className="flex-1 px-4 py-2 text-sm truncate whitespace-normal">
+                <a href='#' className="text-gray-900 font-medium hover:text-gray-600">
+                  {workspace.name}
+                </a>
+                <p className="text-xs text-gray-500">{workspace.description}</p>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+    );
+  }
+}
