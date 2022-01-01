30164f6bffe5bbda975c8b03a145f11d29d95707
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2022-01-01 21:21:58 +0000

    Parse Route file added

--- client/app.jsx
+++ client/app.jsx
@@ -2,8 +2,23 @@ import React from 'react';
 import PageHeader from './components/page-header';
 import WorkspaceForm from './components/workspace-form';
 import WorkspaceListView from './pages/workspace-home';
+import parseRoute from './lib/parse-route';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      route: parseRoute(window.location.hash)
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('hashchange', () => {
+      const changeView = parseRoute(window.location.hash);
+      this.setState({ route: changeView });
+    });
+  }
+
   render() {
     return (
       <>
