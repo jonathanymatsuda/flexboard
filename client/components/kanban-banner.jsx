f7877ecdd18e7ae7e0a51e9fd69927068f24eb60
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2022-01-06 16:40:51 +0000

    Completed feature 5 users can create lists

--- client/components/kanban-banner.jsx
+++ client/components/kanban-banner.jsx
@@ -9,7 +9,7 @@ export default class KanbanBanner extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      boards: '',
+      board: '',
       title: ''
     };
     this.handleChange = this.handleChange.bind(this);
@@ -19,7 +19,7 @@ export default class KanbanBanner extends React.Component {
   componentDidMount() {
     fetch(`/api/boards/${this.props.boardId}`)
       .then(res => res.json())
-      .then(boards => this.setState({ boards }))
+      .then(board => this.setState({ board }))
       .catch(err => console.error(err));
   }
 
@@ -33,14 +33,14 @@ export default class KanbanBanner extends React.Component {
     event.preventDefault();
     const newList = {
       title: this.state.title,
-      boardId: this.state.boards.boardId
+      boardId: this.state.board.boardId
     };
     this.props.onSubmit(newList);
-    this.setState({ listTitle: '' });
+    this.setState({ title: '' });
   }
 
   render() {
-    const { title } = this.state.boards;
+    const { title } = this.state.board;
     return (
       <div className="mt-5 px-8 pb-5 border-b border-gray-200 sm:flex sm:items-center sm:justify-between">
         <h2 className="text-xl leading-6 font-medium text-gray-900">{title}</h2>
